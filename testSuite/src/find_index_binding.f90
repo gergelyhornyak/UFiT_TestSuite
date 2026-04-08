62ba1dd9b4e1172825e3bb5ac54f16d803ea2e9c
gergely hornyak <gergely2125@gmail.com>
2026-04-08 21:34:52 +0200

    first tricky function: had to create a wrapper subroutine to run the function and not segfault the container. Test for find_index function

---
+++ testSuite/src/find_index_binding.f90
@@ -0,0 +1,17 @@
+      subroutine find_index_bridge(grid, sz, target_loc, idx_in, idx_out) bind(C, name="find_index_bridge")
+
+      !Index of gridpoint below ; grid is increasing
+        use iso_c_binding
+    
+        use UFiT_Functions_Fortran, only : find_index 
+
+        integer(c_int), intent(in)  :: sz
+        integer(c_int), intent(in)  :: idx_in
+        real(c_double), intent(in)  :: target_loc
+        real(c_double), intent(in)  :: grid(sz)
+
+        integer(c_int), intent(out) :: idx_out
+
+        idx_out = find_index(grid, sz, target_loc, idx_in)
+
+      end subroutine find_index_bridge
